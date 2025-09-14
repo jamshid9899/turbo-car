34a63d612add1a196cd1a6370faf93ebd83384e3
jamshid98 <jamshid98991213@gmail.com>
2025-09-14 15:05:14 +0900

    feat: create schema model

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: "Member", schema: MemberModule}])
+  ],
   providers: [MemberResolver, MemberService]
 })
 export class MemberModule {}
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class MemberService {
+    constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
     public async signup(): Promise<string> {
         return 'signup executed!';
     }
@@ -11,10 +14,10 @@ export class MemberService {
     }
 
      public async updateMember(): Promise<string> {
-        return 'signup executed!';
+        return 'updateMember executed!';
     }
 
      public async getMember(): Promise<string> {
-        return 'signup executed!';
+        return 'getMember executed!';
     }
 }
