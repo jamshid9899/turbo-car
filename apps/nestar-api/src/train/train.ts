a7b8d0079558acef5033c24a51113a3db619837a
jamshid98 <jamshid98991213@gmail.com>
2025-09-17 17:38:14 +0900

    fix: TaskZM is done

--- apps/nestar-api/src/train/train.ts
+++ apps/nestar-api/src/train/train.ts
@@ -1,7 +1,14 @@
 //console.log('NestJS test file ishladi!');
 //Task ZL
-function stringKebab(str) {
-    return str.trim().toLowerCase().split(/\s+/).join('_')
-}
+// function stringKebab(str) {
+//     return str.trim().toLowerCase().split(/\s+/).join('_')
+// }
 
-console.log(stringKebab("i love Kebab"))
+// console.log(stringKebab("i love Kebab"))
+
+//Task ZM
+function reverseInteger(number) {
+   return Number(number.toString().split("").reverse().join(""));
+}
+console.log(reverseInteger(123456789));
+console.log(reverseInteger(1911));
