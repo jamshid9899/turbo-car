bdcdb56f18cc8a3bf6045955edb7aa5ff69ef730
jamshid98 <jamshid98991213@gmail.com>
2025-09-25 17:37:56 +0900

    feat: Task ZO is done

--- apps/nestar-api/src/train/train.ts
+++ apps/nestar-api/src/train/train.ts
@@ -7,8 +7,28 @@
 // console.log(stringKebab("i love Kebab"))
 
 //Task ZM
-function reverseInteger(number) {
-   return Number(number.toString().split("").reverse().join(""));
+// function reverseInteger(number) {
+//    return Number(number.toString().split("").reverse().join(""));
+// }
+// console.log(reverseInteger(123456789));
+// console.log(reverseInteger(1911));
+
+
+//TASK ZO
+function areParenthesesBalanced(str: string): boolean {
+   let count = 0;
+
+   for (const char of str) {
+      if (char === "(") {
+         count++;
+      } else if (char === ")") {
+         count--;
+         if (count < 0) {
+            return false;
+         }
+      }
+   }
+   return count === 0;
 }
-console.log(reverseInteger(123456789));
-console.log(reverseInteger(1911));
+
+console.log(areParenthesesBalanced("string()ichida(qavslar)soni()balansda"));
