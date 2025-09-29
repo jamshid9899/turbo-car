7e9cef95ab0fb49a9b529a757a21f64c48a9c2ac
jamshid98 <jamshid98991213@gmail.com>
2025-09-30 03:05:20 +0900

    feat: develop getAgentProperties business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -3,7 +3,7 @@ import { PropertyService } from './property.service';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import type { ObjectId } from 'mongoose';
 import { UseGuards } from '@nestjs/common';
@@ -58,6 +58,17 @@ export class PropertyResolver {
      ): Promise<Properties> {
       console.log('Query: getProperties');
       return await this.propertyService.getProperties(memberId, input)
+     }
+
+     @Roles(MemberType.AGENT)
+     @UseGuards(RolesGuard)
+     @Query((returns) => Properties)
+     public async getAgentProperties(
+      @Args('input') input: AgentPropertiesInquiry,
+      @AuthMember('_id') memberId: ObjectId,
+     ): Promise<Properties> {
+      console.log('Query: getAgentProperties');
+      return await this.propertyService.getAgentProperties(memberId, input)
      }
 
 }
