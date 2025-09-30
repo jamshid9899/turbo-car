1cd57aa57fcb0b3acb9d511013d1a3a7831eeb04
jamshid98 <jamshid98991213@gmail.com>
2025-09-30 14:39:43 +0900

    feat: develop GetAllPropertiesByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -3,7 +3,7 @@ import { PropertyService } from './property.service';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import type { ObjectId } from 'mongoose';
 import { UseGuards } from '@nestjs/common';
@@ -72,5 +72,18 @@ export class PropertyResolver {
       console.log('Query: getAgentProperties');
       return await this.propertyService.getAgentProperties(memberId, input)
      }
+
+     /** ADMIN */
+     @Roles(MemberType.ADMIN)
+     @UseGuards(RolesGuard)
+     @Mutation((returns) => Properties)
+     public async getAllPropertiesByAdmin(
+      @Args('input') input: AllPropertiesInquiry,
+      @AuthMember('_id') memberId: ObjectId,
+     ): Promise<Properties> {
+      console.log('Query: getAllPropertiesByAdmin');
+      return await this.propertyService.getAllPropertiesByAdmin(input)
+     }
+
 
 }
