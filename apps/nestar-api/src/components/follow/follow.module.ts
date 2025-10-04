e0ce5710897de79282640a5385bd84108108bdaa
jamshid98 <jamshid98991213@gmail.com>
2025-10-04 12:02:18 +0900

    feat: create follow related resolver and service model

--- apps/nestar-api/src/components/follow/follow.module.ts
+++ apps/nestar-api/src/components/follow/follow.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MemberModule } from '../member/member.module';
+import { AuthModule } from '../auth/auth.module';
+import { FollowResolver } from './follow.resolver';
+import { FollowService } from './follow.service';
+import FollowSchema from '../../schemas/Follow.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+    imports: [
+    MongooseModule.forFeature([{ name: "Follow", schema: FollowSchema }]), 
+    AuthModule,
+    MemberModule
+  ],
+  providers: [FollowResolver, FollowService],
+  exports: [FollowService],
+})
 export class FollowModule {}
