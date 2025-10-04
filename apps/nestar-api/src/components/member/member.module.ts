421c184eafdd5c3c8a3a7e2a4c8cdc36c4165b82
jamshid98 <jamshid98991213@gmail.com>
2025-10-04 15:19:56 +0900

    fix: modify getMember business logic

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -6,9 +6,14 @@ import MemberSchema  from '../../schemas/Member.model'
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { LikeModule } from '../like/like.module';
+import FollowSchema from '../../schemas/Follow.model';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: "Member", schema: MemberSchema }]), //schema 
+    MongooseModule.forFeature([{ name: "Follow", schema: FollowSchema }]), //schema 
+    
+    
+
     AuthModule,
     ViewModule,
     LikeModule
